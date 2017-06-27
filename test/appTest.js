34b32b2a6615f3e8e11ba8120eedd6dc50102625
Robert Saunders <rtsaunders19@gmail.com>
2017-06-27 14:17:13 -0500

    separated tests

--- test/appTest.js
+++ test/appTest.js
@@ -8,7 +8,6 @@ describe('App', function() {
     assert.equal(result, 'hello');
   });
 
-
   it('sayHello should return type string', function() {
     let result = sayHello()
     assert.typeOf(result, 'string');
@@ -25,10 +24,36 @@ describe('App', function() {
   });
 });
 
+
+const app = require('../app');
+const sayHelloResult = app.sayHello()
+const addNumbersResult = app.addNumbers(5,5)
+
+describe('App', function() {
+  describe('sayHello()', function() {
+    it('sayHello should return hello', function() {
+      assert.equal(sayHelloResult, 'hello');
+    });
+    it('sayHello should return type string', function() {
+      assert.typeOf(sayHelloResult, 'string');
+    });
+  });
+  describe('addNumbers()', function() {
+    it('addNumbers should be greater than 5', function() {
+      assert.isAbove(addNumbersResult, 5);
+    })
+    it('addNumbers should return type number', function() {
+      assert.typeOf(addNumbersResult, 'number');
+    });
+  });
+})
+
 /*
 if function in app.js isn't stored as a method
+
+const sayHelloResult = app.sayHello()
 describe('App', function() {
   it('sayHello should return hello', function() {
-    assert.equal(app(), 'hello');
+    assert.equal(sayHelloResult, 'hello');
   });
 */
